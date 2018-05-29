159a1f39bcd2b0f063fb3f1dbc4a5f457c4388a1
derrick b <dbozich1@gmail.com>
2018-05-29 07:03:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,42 +24,39 @@ class School
   def sort
     hash = {}
     @roster.each do |grade, student|
-
-
-
         if hash.keys.include?(grade) == false
           hash[grade]=[]
         end
         hash[grade]= @roster[grade].sort
-      end
-      hash
+     end
+     @roster = hash
 
-
-    end
-    @roster = hash
 
   end
+    
+
+  
 
 end
 
-school = School.new("Bayside High School")
-roster = school.roster
-# school.grade(9)
-# school.add_student("trey", 9)
-# school.add_student("Jerry", 9)
-
-school.add_student("Homer Simpson", 9)
-school.add_student("Bart Simpson", 9)
-school.add_student("Avi Flombaum", 10)
-school.add_student("Jeff Baird", 10)
-school.add_student("Blake Johnson", 7)
-school.add_student("Jack Bauer", 7)
-
-# hash = {}
-# roster.each do |g,s|
-#   if hash.keys.include?(g) == false
-#     hash[g]=[]
-#   end
-#   hash[g]= roster[g].sort
-# end
-# hash
+# school = School.new("Bayside High School")
+# roster = school.roster
+# # school.grade(9)
+# # school.add_student("trey", 9)
+# # school.add_student("Jerry", 9)
+# 
+# school.add_student("Homer Simpson", 9)
+# school.add_student("Bart Simpson", 9)
+# school.add_student("Avi Flombaum", 10)
+# school.add_student("Jeff Baird", 10)
+# school.add_student("Blake Johnson", 7)
+# school.add_student("Jack Bauer", 7)
+# 
+# # hash = {}
+# # roster.each do |g,s|
+# #   if hash.keys.include?(g) == false
+# #     hash[g]=[]
+# #   end
+# #   hash[g]= roster[g].sort
+# # end
+# # hash
