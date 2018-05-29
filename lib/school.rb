6bf8b88d55ab056781671b759894290340ec05c1
derrick b <dbozich1@gmail.com>
2018-05-29 06:30:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,9 +24,9 @@ class School
   def sort
     @roster.each do |grade, student|
 
-      grade(grade).sort
+      # grade(grade).sort
 
-       #student.sort
+       student[grade].sort
     end
     @roster
   end
